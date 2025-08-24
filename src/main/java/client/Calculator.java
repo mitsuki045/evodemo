3deb41e923b4b490e48230ea267c1650a75cc5ba
mitsuki045 <22ts045@m.nanzan-u.ac.jp>
2025-08-24 22:11:30 +0900

    全部試すライブラリa

--- src/main/java/client/Calculator.java
+++ src/main/java/client/Calculator.java
@@ -1,11 +1,71 @@
+// ファイル: example/Calculator.java
 package example;
 
+import java.io.IOException;
+
 public class Calculator {
-    public double add(int a, double b) {
+
+    // REMOVE METHOD 対象
+    public int add(int a, int b) {
         return a + b;
     }
 
-    public boolean isPositive(int n) {
-        return n > 0;
+    // LOST VISIBILITY 対象
+    public void publicMethod() {
+        System.out.println("public method");
     }
+
+    // CHANGE IN RETURN TYPE 対象
+    public int returnInt() {
+        return 42;
+    }
+
+    // CHANGE IN PARAMETER LIST 対象
+    public int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    // CHANGE IN EXCEPTION LIST 対象
+    public void mayThrow() throws IOException {
+        throw new IOException("error");
+    }
+
+    // ADD FINAL MODIFIER 対象
+    public void overridable() {
+        System.out.println("can override");
+    }
+
+    // REMOVE STATIC MODIFIER 対象
+    public static void staticMethod() {
+        System.out.println("static method");
+    }
+
+    // MOVE METHOD 対象
+    public void moveMe() {
+        System.out.println("moved method");
+    }
+
+    // RENAME METHOD 対象
+    public void renameMe() {
+        System.out.println("old name");
+    }
+
+    // PUSH DOWN METHOD 対象
+    public void pushDown() {
+        System.out.println("in superclass");
+    }
+
+    // INLINE METHOD 対象
+    public void inlineCaller() {
+        inlineCallee();
+    }
+
+    public void inlineCallee() {
+        System.out.println("callee executed");
+    }
+}
+
+// PUSH DOWN METHOD 用のサブクラス
+package example;
+public class SubCalculator extends Calculator {
 }
