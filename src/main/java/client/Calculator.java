584a39660edde5feb838fd2d26bf07e8ceea78e5
Mitsuki <mitsuki.morishita01@gmail.com>
2025-08-31 04:02:18 +0900

    コミットメッセージ（例：2つのJavaファイルを変更)+move

--- src/main/java/client/Calculator.java
+++ src/main/java/client/Calculator.java
@@ -18,15 +18,7 @@ public class Calculator {
     
     
 
-    public int calminus(int x, int y) {
-        return x-y; 
-    }
-
-
-    protected static final int calmaltiply(int x, int y){
-        return x*y;
-    }
-
+    
     
     public int caltwo(int a,int b, int c){
         if(a>b) return 100+c;
--- src/main/java/client/Move.java
+++ src/main/java/client/Move.java
@@ -1,5 +1,15 @@
 package example;
 
 public class Move {
+
+    public int calminus(int x, int y) {
+        return x-y; 
+    }
+
+
+    public int calmaltiply(int x, int y){
+        return x*y;
+    }
+
 }
     
