b3aa958061d8c9f79d78ef495b86d9f522f927f4
mitsuki045 <22ts045@m.nanzan-u.ac.jp>
2025-08-30 23:24:57 +0900

    Update Calculator.java

--- src/main/java/client/Calculator.java
+++ src/main/java/client/Calculator.java
@@ -1,10 +1,11 @@
 package example;
 
 public class Calculator {
-    public void foo() {
-        System.out.println("foo in Calculator");
-    }
+    // foo() はここから削除された
 }
 
 public class AdvancedCalculator extends Calculator {
+    public void foo() {
+        System.out.println("foo in Calculator (superclass)");
+    }
 }
