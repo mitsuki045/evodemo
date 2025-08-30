e49fe7d3bdd9721cc5970415cb9d31b2d855bedf
Mitsuki <mitsuki.morishita01@gmail.com>
2025-08-31 02:50:58 +0900

    コミットメッセージ（例：2つのJavaファイルを変更)+downme

--- src/main/java/client/Engineer.java
+++ src/main/java/client/Engineer.java
@@ -11,6 +11,9 @@ public class Engineer extends Calculator {
         return skill;
     }
 
+    protected final double add(double a, int b) {
+        return a + b;
+    }
     
     // Calculator の protected フィールド・メソッドは使用可能
     public int calculateSum(int a, int b) {
