f7a73b3c1e04d7e6a4d314f48ca40c2400de9c28
Mitsuki <mitsuki.morishita01@gmail.com>
2025-08-31 04:01:20 +0900

    コミットメッセージ（例：2つのJavaファイルを変更)+downme

--- src/main/java/client/Calculator.java
+++ src/main/java/client/Calculator.java
@@ -18,7 +18,14 @@ public class Calculator {
     
     
 
-    
+    public int calminus(int x, int y) {
+        return x-y; 
+    }
+
+
+    protected static final int calmaltiply(int x, int y){
+        return x*y;
+    }
 
     
     public int caltwo(int a,int b, int c){
