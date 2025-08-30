0a45d4653f70520f8d10af573ca1f142850ca6cf
mitsuki045 <22ts045@m.nanzan-u.ac.jp>
2025-08-30 23:31:16 +0900

    push down よそサイトライブラリa

--- src/main/java/client/Calculator.java
+++ src/main/java/client/Calculator.java
@@ -1,11 +1,1 @@
-package example;
 
-public class Calculator {
-    // foo() はここから削除された
-}
-
-public class AdvancedCalculator extends Calculator {
-    public void foo() {
-        System.out.println("foo in Calculator (superclass)");
-    }
-}
