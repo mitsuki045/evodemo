03e8fb9a954f07e9a52607eb8109443f0963d99f
mitsuki045 <22ts045@m.nanzan-u.ac.jp>
2025-08-30 23:31:39 +0900

    push よそサイトライブラリa

--- src/main/java/client/Calculator.java
+++ src/main/java/client/Calculator.java
@@ -1,1 +1,25 @@
 
+package package1;
+
+public class User {
+    /**
+     * ユーザID
+     */
+    private int UserId;
+
+    /**
+     * ユーザID設定
+     * @param userID
+     */
+    public void SetUserId(int userID) {
+        this.UserId = userID;
+    }
+
+    /**
+     * ユーザID取得
+     * @return
+     */
+    public int GetUserId() {
+        return UserId;
+    }
+}
