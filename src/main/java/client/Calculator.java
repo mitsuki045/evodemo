0187501075b4058b28d433c90f9e967a13809981
mitsuki045 <22ts045@m.nanzan-u.ac.jp>
2025-08-31 19:35:56 +0900

    エラー＋BC

--- src/main/java/client/Calculator.java
+++ src/main/java/client/Calculator.java
@@ -19,19 +19,19 @@ public class Calculator {
     // 計算メソッド
     
     
-    protected double caltwo(double a,int b, int c, int y,int x){
+    protected double caltwo(double a,int b, int c, int y,int x, int z){
         if(c>a) return 100+c;
         if(b>a) return 50+c; 
         return 0+c;
     }
 
-    protected static final int newcalmaltiply(int x,int y,int a, int b, int c){
+    public int newcalmaltiply(int x,int y,int a, int b){
         return x*y;
     }
     
-    public void mayThrow() throws SQLException {
+    protected void mayThrow() throws IOException {
         // 例外を発生させるサンプル処理
-        throw new SQLException("SQLエラー");
+        throw new IOException("ファイルエラー");
     }
 
 }
