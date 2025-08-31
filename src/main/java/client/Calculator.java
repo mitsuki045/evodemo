55bbf32bc92184795a85333525cf27d3373eb755
mitsuki045 <22ts045@m.nanzan-u.ac.jp>
2025-08-31 20:11:23 +0900

    Update Calculator.java

--- src/main/java/client/Calculator.java
+++ src/main/java/client/Calculator.java
@@ -19,13 +19,13 @@ public class Calculator {
     // 計算メソッド
     
     
-    protected double newcaltwo(double a,int b, int c){
+    public double newcaltwo(double a,int b, int c){
         if(c>a) return 100+c;
         if(b>a) return 50+c; 
         return 0+c;
     }
 
-        protected final double newcalmaltiply(int x,int y){
+        public double newcalmaltiply(int x,int y){
         return x*y;
     }
     
