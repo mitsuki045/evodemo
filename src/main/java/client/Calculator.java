7dfd7d958b0047ee6ee8075cddb14ab4db49efd7
mitsuki045 <22ts045@m.nanzan-u.ac.jp>
2025-08-31 20:09:13 +0900

    ふくすうBC （static除く

--- src/main/java/client/Calculator.java
+++ src/main/java/client/Calculator.java
@@ -25,7 +25,7 @@ public class Calculator {
         return 0+c;
     }
 
-        public int newcalmaltiply(int x,int y){
+        protected final double newcalmaltiply(int x,int y){
         return x*y;
     }
     
