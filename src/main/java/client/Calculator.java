3ef9a48f6baac28d90491bfabd755b6ddd61754f
mitsuki045 <22ts045@m.nanzan-u.ac.jp>
2025-08-31 21:11:44 +0900

    rename+引数増やし+int→double

--- src/main/java/client/Calculator.java
+++ src/main/java/client/Calculator.java
@@ -19,13 +19,13 @@ public class Calculator {
     // 計算メソッド
     
     
-    public final double newcaltwo(double a,int b, int c){
+    public final double caltwo(double a,int b, int c,int x, int y){
         if(c>a) return 100+c;
         if(b>a) return 50+c; 
         return 0+c;
     }
 
-    public int newcalmaltiply(int x,int y){
+    public int calmaltiply(double x,int y,int a,int y){
         return x*y;
     }
     
