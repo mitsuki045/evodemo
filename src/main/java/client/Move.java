efa072d90f4619623b8509d923d9ecf689e39f8c
mitsuki045 <22ts045@m.nanzan-u.ac.jp>
2025-08-31 22:09:25 +0900

    BC+パラメータ変更

--- src/main/java/client/Move.java
+++ src/main/java/client/Move.java
@@ -2,13 +2,13 @@ package example;
 
 public class Move {
 
-    public final double newcaltwo(double a,double b, int c,int x,int y){
+    public final int newcaltwo(double a,double b, int c){
         if(c>a) return 100+c;
         if(b>a) return 50+c; 
         return 0+c;
     }
 
-    public double newcalmaltiply(double x,double y){
+    protected double newcalmaltiply(double x,double y,int a){
         return x*y;
     }
 
