de7abdc123f5883738fa0084fb781ddf5c89b10b
mitsuki045 <22ts045@m.nanzan-u.ac.jp>
2025-09-05 21:51:43 +0900

    Create Abstract.java

---
+++ src/main/java/client/Abstract.java
@@ -0,0 +1,11 @@
+package example;
+
+public class Abstract {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
