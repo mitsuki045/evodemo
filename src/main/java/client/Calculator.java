53c3d2af31ecf22b2095e36367d053a8a0654d2d
mitsuki045 <22ts045@m.nanzan-u.ac.jp>
2025-09-09 17:13:55 +0900

    中間発表資料用ライブラリB

--- src/main/java/client/Calculator.java
+++ src/main/java/client/Calculator.java
@@ -1,8 +1,8 @@
 package example;
 
 public class Calculator {
-    public int add(int a, int b) {
-        return a + b;
+    public int newadd(int a, int b) {
+        return a + b+1;
     }
 
     public boolean isPositive(int n) {
