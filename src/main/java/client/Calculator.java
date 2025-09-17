b67f56e3f14f41355c8240faf7ef12cc4e623acc
mitsuki045 <22ts045@m.nanzan-u.ac.jp>
2025-09-17 15:21:47 +0900

    Update Calculator.java

--- src/main/java/client/Calculator.java
+++ src/main/java/client/Calculator.java
@@ -1,11 +1,15 @@
 package example;
 
 public class Calculator {
-    public int newadd(int a, int b) {
-        return a + b+1;
+    public int add(int a, int b) {
+        return a + b;
     }
 
     public boolean isPositive(int n) {
         return n > 0;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
     }
 }
