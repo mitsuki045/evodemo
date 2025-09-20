3f9c53bf3feda6bb4376f0948c8aa9519698cd84
mitsuki045 <22ts045@m.nanzan-u.ac.jp>
2025-09-21 00:18:11 +0900

    名前変更＋振る舞い変更

--- src/main/java/client/Calculator.java
+++ src/main/java/client/Calculator.java
@@ -1,8 +1,8 @@
 package example;
 
 public class Calculator {
-    public int add(int a, int b) {
-        return a + b;
+    public int newadd(int a, int b) {
+        return a + b + 1;
     }
 
     private int sub(int a, int b) {
