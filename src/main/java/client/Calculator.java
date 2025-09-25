6991afc378f9a8cb0a3cf5382c13789dc917b349
mitsuki045 <22ts045@m.nanzan-u.ac.jp>
2025-09-25 14:56:55 +0900

    diffように引数変更と新しいの増やし

--- src/main/java/client/Calculator.java
+++ src/main/java/client/Calculator.java
@@ -1,11 +1,15 @@
 package example;
 
 public class Calculator {
-    public int newadd(int a, int b) {
+    public int newadd(int a, double b) {
         return a + b + 1;
     }
 
-    private double sub(int a, int b) {
+    private double sub(int a, int b int c) {
         return a - b;
+    }
+
+    private double sub2(int a, int b) {
+        return a - b -5;
     }
 }
