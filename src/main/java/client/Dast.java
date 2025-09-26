becc3b5d0d22e76d45cd33ae33cb27c1f9b9d551
mitsuki045 <22ts045@m.nanzan-u.ac.jp>
2025-09-27 00:41:01 +0900

    Dastにmultiply追加

--- src/main/java/client/Dast.java
+++ src/main/java/client/Dast.java
@@ -4,6 +4,7 @@ public class Dast {
     public int add(int a, int b) {
         return a + b;
     }
-    public abstract int multiply(int a, int b);
+    public abstract int multiply(int a, int b){
+    }
 }
 
