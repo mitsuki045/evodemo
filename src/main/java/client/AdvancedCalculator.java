4af0b2a0e0956d374426a70e138efa3cd5668647
mitsuki045 <22ts045@m.nanzan-u.ac.jp>
2025-09-27 00:17:59 +0900

    継承の名前変えてみた？

--- src/main/java/client/AdvancedCalculator.java
+++ src/main/java/client/AdvancedCalculator.java
@@ -1,12 +1,9 @@
 package example;
 
-public class AdvancedCalculator extends Dast {
+public class Advanced extends Dast {
     @Override
     public int multiply(int a, int b) {
         return a * b;
     }
 
-    public int addnew(int a, int b){
-        return a+b+10;
-    }
 }
