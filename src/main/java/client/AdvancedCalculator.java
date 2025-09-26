ef9bdb9b804efddff3708d3ea71caf039a015fc9
mitsuki045 <22ts045@m.nanzan-u.ac.jp>
2025-09-27 00:41:45 +0900

    Update AdvancedCalculator.java

--- src/main/java/client/AdvancedCalculator.java
+++ src/main/java/client/AdvancedCalculator.java
@@ -1,11 +1,11 @@
 package example;
 
-public class AdvancedCalculator extends Dast {
+public class AdvancedCalculator extends Calculator {
     @Override
     public int multiply(int a, int b) {
         return a * b;
     }
-    public int add(int a,int b){
+    public int addnew(int a,int b){
         return a+b+10;
     }
 
