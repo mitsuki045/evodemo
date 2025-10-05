42000be1b1e7df215b57a6cadf88ca33db3640a1
mitsuki045 <22ts045@m.nanzan-u.ac.jp>
2025-10-05 23:24:36 +0900

    Update Calculator.java

--- src/main/java/client/Calculator.java
+++ src/main/java/client/Calculator.java
@@ -5,7 +5,7 @@ public class Calculator {
     public int multiply(int a, int b) {
     }
     
-    public int add(int a, int b) {
+    public int newadd(int a, int b) {
         return a + b + 1;
     }
 
