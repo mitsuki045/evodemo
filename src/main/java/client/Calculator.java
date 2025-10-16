471e21fb06fe46dac8e9cc80b8c3a1050f93b1ef
mitsuki045 <22ts045@m.nanzan-u.ac.jp>
2025-10-16 15:48:45 +0900

    Update Calculator.java

--- src/main/java/client/Calculator.java
+++ src/main/java/client/Calculator.java
@@ -12,8 +12,4 @@ public class Calculator {
     private double sub(int a, int b, int c) {
         return a - b;
     }
-
-    private double sub2(int a, int b) {
-        return a - b -5;
-    }
 }
